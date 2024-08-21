2341cc3d8b13f2ef60fece775f86442d91067893
snehalpati1 <snehalsp99@gmail.com>
2024-08-21 21:16:35 +0530

    v1:B-123486,TestCommitWithGUI

---
+++ src/main/java/com/SnehalIT/CrudRepositoryMethods/service/StudentServiceImpl 1.java
@@ -0,0 +1,111 @@
+package com.SnehalIT.CrudRepositoryMethods.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.SnehalIT.CrudRepositoryMethods.dao.StudentRepositroy;
+import com.SnehalIT.CrudRepositoryMethods.entity.Student;
+@Service
+public class StudentServiceImpl implements StudentServiceI{
+
+	@Autowired
+	private StudentRepositroy studentRepository;
+
+	@Override
+	public Student saveStudent(Student stu) {
+		Student student=studentRepository.save(stu);
+		return student;
+	}
+
+	@Override
+	public List<Student> saveAllStudents(List<Student> students) {
+		List<Student>saveAll=(List<Student>)studentRepository.saveAll(students);
+		return saveAll;
+	}
+	
+	@Override
+	public List<Student> getAllRecords() {
+		List<Student> all = (List<Student>) studentRepository.findAll();
+		return all;
+	}
+
+	@Override
+	public List<Student> getAllStuMPK(List<Integer> list) {
+	List<Student> allById = (List<Student>) studentRepository.findAllById(list);
+		return allById;
+	}
+
+	@Override
+	public boolean existsByIdStu(Integer id) {
+	boolean existsById = studentRepository.existsById(id);
+		return existsById;
+	}
+
+	@Override
+	public long countNoOfRecords() {
+		long count = studentRepository.count();
+		return count;
+	}
+
+	@Override
+	public Student getStudentById(Integer id) {
+	Student byId = studentRepository.findById(id).get();
+		return byId;
+	}
+
+	@Override
+	public void deleteRecordById(Integer id) {
+	studentRepository.deleteById(id);
+		
+	}
+
+	@Override
+	public void deleteRecordByObject(Student stu) {
+	studentRepository.delete(stu);
+		
+	}
+
+	@Override
+	public void deleteStuByMPK(List<Integer> list) {
+	studentRepository.deleteAllById(list);
+		
+	}
+
+	@Override
+	public void deleteAllRecords() {
+		studentRepository.deleteAll();
+		
+}
+
+
+	//This is Witten by QA Team
+	public void TestingTeam() {
+		System.out.println("THis is the QA tester team");
+	}
+	
+	//This is Witten by QA Team
+		public void QATeams() {
+			System.out.println("THis is the QA tester team");
+		}
+
+
+
+	//this is commit by the dev
+	public void developerCode() {
+		System.out.println("This is developer code");
+	}
+
+	
+	//this is commit by the dev
+		public void deveCode() {
+			System.out.println("This is developer code");
+}
+
+		//this is commit by the developer
+			public void deveCodeScenariosConflicts() {
+				System.out.println("This is developer code");
+			}
+}
+
